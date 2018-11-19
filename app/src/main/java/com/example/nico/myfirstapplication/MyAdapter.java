a13f5e288c63a375cf5c4c90b672cf8cefa34d2f
NHURTEVENT <nicolas.hurtevent@viacesi.fr>
2018-11-19 23:11:27 +0100

    Writes infos in log file

--- app/src/main/java/com/example/nico/myfirstapplication/MyAdapter.java
+++ app/src/main/java/com/example/nico/myfirstapplication/MyAdapter.java
@@ -1,6 +1,8 @@
 package com.example.nico.myfirstapplication;
 
 import android.content.Context;
+import android.os.Environment;
+import android.text.Editable;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -11,7 +13,14 @@ import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MyAdapter extends BaseAdapter {
@@ -20,7 +29,7 @@ public class MyAdapter extends BaseAdapter {
     private Context context;
 
 
-    public MyAdapter(Context context, ArrayList<Tuple> list){
+    public MyAdapter(Context context, ArrayList<Tuple> list) {
         this.context = context;
         this.list = list;
     }
@@ -44,10 +53,9 @@ public class MyAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         final View row;
         final ListViewHolder listViewHolder;
-        if(convertView == null)
-        {
+        if (convertView == null) {
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = layoutInflater.inflate(R.layout.activity_custom_listview,parent,false);
+            row = layoutInflater.inflate(R.layout.activity_custom_listview, parent, false);
             listViewHolder = new ListViewHolder();
             listViewHolder.wifiInfo = row.findViewById(R.id.wifiInfo);
             listViewHolder.location = row.findViewById(R.id.location);
@@ -55,8 +63,8 @@ public class MyAdapter extends BaseAdapter {
             final EditText text = row.findViewById(R.id.location);
             final TextView infoWifi = row.findViewById(R.id.wifiInfo);
             final TextView distance = row.findViewById(R.id.distance);
-            text.setOnEditorActionListener(new EditText.OnEditorActionListener(){
-                            @Override
+            text.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+                @Override
                 public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                     if (actionId == EditorInfo.IME_ACTION_SEARCH ||
                             actionId == EditorInfo.IME_ACTION_DONE ||
@@ -65,8 +73,8 @@ public class MyAdapter extends BaseAdapter {
                                     event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
                         if (event == null || !event.isShiftPressed()) {
                             // the user is done typing.
-                            
-                            Log.d("INFO", String.valueOf(infoWifi.getText())+" "+String.valueOf(text.getText())+" "+String.valueOf(distance.getText()));
+                            writeInFile(String.valueOf(infoWifi.getText()), String.valueOf(text.getText()), String.valueOf(distance.getText()));
+                            //Log.d("INFO", String.valueOf(infoWifi.getText())+" "+String.valueOf(text.getText())+" "+String.valueOf(distance.getText()));
                             return true; // consume.
                         }
                     }
@@ -74,11 +82,9 @@ public class MyAdapter extends BaseAdapter {
                 }
             });
             row.setTag(listViewHolder);
-        }
-        else
-        {
-            row=convertView;
-            listViewHolder= (ListViewHolder) row.getTag();
+        } else {
+            row = convertView;
+            listViewHolder = (ListViewHolder) row.getTag();
         }
         final Tuple tuple = getItem(position);
 
@@ -87,5 +93,38 @@ public class MyAdapter extends BaseAdapter {
         listViewHolder.distance.setText(tuple.distance);
 
         return row;
+    }
+
+    private void writeInFile(String BSSID, String location, String distance) {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "InfosWifi");
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            File info = new File(file, "info1.txt");
+            if (!info.exists()) {
+                FileWriter writer = new FileWriter(info);
+                writer.append("{" + (BSSID.substring(0, BSSID.length() - 4)) + "," + BSSID.substring(BSSID.length() - 4) + "," + location + "," + distance + "}\n");
+                writer.flush();
+                writer.close();
+            } else {
+                StringBuilder txt = new StringBuilder();
+                BufferedReader br = new BufferedReader(new FileReader(info));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    txt.append(line);
+                    txt.append('\n');
+                }
+                br.close();
+                String str = txt.toString();
+                FileWriter writer = new FileWriter(info);
+                writer.append(str + "{" + (BSSID.substring(0, BSSID.length() - 4)) + "," + BSSID.substring(BSSID.length() - 4) + "," + location + "," + distance + "}\n");
+                writer.flush();
+                writer.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
